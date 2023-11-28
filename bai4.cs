cda3b62395d496aa655e7e42c5a0431aae85abc3
TruongDinhQuan <151931866+TruongDinhQuan@users.noreply.github.com>
2023-11-28 21:35:47 +0700

    Add files via upload

---
+++ bai4.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai_4_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int t;
+            Console.OutputEncoding = Encoding.UTF8;
+            do
+            {
+                Console.Write("Nhập năm: ");
+                t = Convert.ToInt32(Console.ReadLine());
+                if (t > 0)
+                {
+                    if (t % 4 == 0 )
+                    {
+                        if (t % 100 == 0)
+                        {
+                            if (t % 400 == 0)
+                            {
+                                Console.WriteLine("{0} là năm nhuận", t);
+                            }
+                            else { Console.WriteLine("{0} là năm không nhuận", t); }
+                        }  
+                        else { Console.WriteLine("{0} là năm nhuận", t); }
+                    }    
+                    else { Console.WriteLine("{0} là năm không nhuận", t); }
+                }
+                else { Console.WriteLine("nhập sai năm rồi , nhập lại"); }
+            }while (t < 0);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ bai5.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bài_5_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double t;
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("Chương trình xắp xếp học lực của sinh viên");
+            Console.WriteLine("/n");
+            do
+            {
+                Console.Write("Nhap diem: ");
+                t = double.Parse(Console.ReadLine());
+                if (0 <= t && t <= 10)
+                {
+                    if (t >= 9)
+                    {
+                        Console.WriteLine("xuất sắc");
+                    }
+                    else if (t >= 8)
+                        {
+                            Console.WriteLine("Giỏi");
+                        }
+                        else if (t >= 6.5)
+                            {
+                                Console.WriteLine("Khá");
+                            }
+                            else if (t >= 5)
+                                {
+                                    Console.WriteLine("Trung Bình");
+                                }
+                                else if (t >= 3.5)
+                                    {
+                                        Console.WriteLine("Yếu");
+                                    }
+                                    else {   Console.WriteLine("kém");  }
+                }
+                else {  Console.WriteLine("nhập sai");  }
+            } while (0 >= t || t >= 10);
+            Console.ReadKey();
+        }
+    }
+}
